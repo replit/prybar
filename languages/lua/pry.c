c029e474b69e2e4d61025b0e4d3a8be5ea7d24b0
mason clayton <masongclayton@gmail.com>
2019-02-20 15:46:38 -0800

    fix lua to be compatible with replit

--- languages/lua/pry.c
+++ languages/lua/pry.c
@@ -5,42 +5,12 @@
 
 #include "pry.h"
 
-char* pry_prompt = "> ";
-char* pry_prompt2 = ">> ";
 lua_State *pry_L;
 
-const char * pry_get_version() {
-	return LUA_COPYRIGHT;
-}
+const char *pry_get_version(void) { return LUA_RELEASE "  " LUA_COPYRIGHT; }
 
+void pry_eval(const char *code) { dostring(pry_L, code, "<eval>"); }
 
-void pry_init() {
-	pry_L = luaL_newstate();
-	luaL_openlibs(pry_L);
-}
+void pry_do_repl(void) { dotty(pry_L); }
 
-void pry_eval(const char *code) {
-	luaL_dostring(pry_L, code);
-}
-
-
-int lua_main (int argc, char **argv);
-
-void pry_main() {
-	char* args[2] = {"lua", NULL};
-	lua_main(0, args);
-}
-
-int handle_script (lua_State *L, char **argv, int n);
-void pry_eval_file(const char* file, int argn, const char *argv) {
-	int status, result;
-	const char* xargv[argn+1];
-	const char* ptr = argv;
-	for (int i = 0; i < argn; ++i) {
-		xargv[i] = ptr;
-		ptr += strlen(ptr) + 1;
-	}
-	xargv[argn] = NULL;
-
-	handle_script(pry_L, xargv, 0);
-}
+void pry_eval_file(char *file) { handle_script(pry_L, &file, 0); }
