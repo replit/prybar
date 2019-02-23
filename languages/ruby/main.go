6ddcc27a5fe3690e3fd1d7ae7804440a9ab3774f
mason clayton <masongclayton@gmail.com>
2019-02-22 16:51:38 -0800

    allow setting the prompt

--- languages/ruby/main.go
+++ languages/ruby/main.go
@@ -13,38 +13,77 @@ import (
 )
 
 type Ruby struct {
+	ps1 string
+	ps2 string
 }
 
-func (p Ruby) Open() {
+func (p *Ruby) Open() {
 	C.pry_open()
 }
 
-func (p Ruby) Version() string {
+func (p *Ruby) Version() string {
 	return C.GoString(C.pry_ruby_version())
 }
 
-func (p Ruby) Eval(code string) {
+func (p *Ruby) SetPrompts(ps1, ps2 string) {
+	p.ps1 = ps1
+	p.ps2 = ps2
+}
+
+func (p *Ruby) Eval(code string) {
 	p.EvalExpression(code)
 }
 
-func (p Ruby) EvalExpression(code string) string {
+func (p *Ruby) EvalExpression(code string) string {
 	ccode := C.CString(code)
 	defer C.free(unsafe.Pointer(ccode))
 	res := C.pry_eval(ccode)
 	return C.GoString(res)
 }
 
-func (p Ruby) EvalFile(file string, args []string) {
+func (p *Ruby) EvalFile(file string, args []string) {
 	cfile := C.CString(file)
 	defer C.free(unsafe.Pointer(cfile))
 	C.pry_eval_file(cfile)
 }
 
-func (p Ruby) REPL() {
-	p.Eval("require 'irb'\nbinding.irb")
+func (p *Ruby) REPL() {
+	cps1 := C.CString(p.ps1)
+	cps1n := C.CString("$REPL_PS1")
+
+	defer C.free(unsafe.Pointer(cps1))
+	defer C.free(unsafe.Pointer(cps1n))
+
+	cps2 := C.CString(p.ps2)
+	cps2n := C.CString("$REPL_PS2")
+
+	defer C.free(unsafe.Pointer(cps2))
+	defer C.free(unsafe.Pointer(cps2n))
+
+	vps1 := C.rb_str_new_cstr(cps1)
+	vps2 := C.rb_str_new_cstr(cps2)
+
+	C.rb_gv_set(cps1n, vps1)
+	C.rb_gv_set(cps2n, vps2)
+
+	p.Eval(`
+require 'irb'
+STDOUT.sync = true
+IRB.setup nil
+IRB.conf[:PROMPT][:PRYBAR] = {
+  :PROMPT_I => $REPL_PS1,
+  :PROMPT_N => $REPL_PS1,
+  :PROMPT_S => $REPL_PS2,
+  :PROMPT_C => $REPL_PS2,
+  :RETURN => "=> %s\n"
 }
-func (p Ruby) Close() {
+IRB.conf[:PROMPT_MODE] = :PRYBAR
+irb = IRB::Irb.new
+irb.run IRB.conf`)
+}
+
+func (p *Ruby) Close() {
 	C.ruby_cleanup(0)
 }
 
-var Instance = Ruby{}
+var Instance = &Ruby{}
--- languages/ruby/pry_ruby.h
+++ languages/ruby/pry_ruby.h
@@ -4,5 +4,4 @@
 void pry_open();
 const char *pry_ruby_version();
 char *pry_eval(const char *code);
-void pry_eval_file(const char *file);
-
+void pry_eval_file(char *file);
