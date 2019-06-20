f32ff659d8b9624ae870e8febe7d0e1d5d365560
Sidney Kochman <sidney@kochman.org>
2019-06-20 17:47:17 -0400

    support evaluating queries and returning values

--- languages/sqlite/main.go
+++ languages/sqlite/main.go
@@ -6,22 +6,22 @@ import (
 	"database/sql"
 	"fmt"
 	"io/ioutil"
+	"strings"
 
 	_ "github.com/mattn/go-sqlite3"
 
 	"github.com/replit/prybar/utils"
 )
 
-var Instance = SQLite{}
+var Instance = &SQLite{}
 
 // some compile-time assertions that we satisfy interfaces
-var _ utils.PluginBase = SQLite{}
-var _ utils.PluginEval = SQLite{}
-var _ utils.PluginEvalExpression = SQLite{}
+var _ utils.PluginBase = &SQLite{}
+var _ utils.PluginEval = &SQLite{}
+var _ utils.PluginEvalExpression = &SQLite{}
 
 
 type SQLite struct {
-	open bool
 	db *sql.DB
 }
 
@@ -54,38 +54,97 @@ func (s SQLite) Version() string {
 	return "SQLite version TODO"
 }
 
-func (s SQLite) Open() {
+func (s *SQLite) Open() {
 	db, err := sql.Open("sqlite3", ":memory:")
 	if err != nil {
 		panic(err)
 	}
 	s.db = db
-	s.open = true
 }
 func (s SQLite) Close() {
 	s.db.Close()
 }
 
 func (s SQLite) Eval(line string) {
-	if !s.open {
-		s.Open()
-	}
 	_, err := s.db.Exec(line)
 	if err != nil {
 		panic(err)
 	}
 }
 
-func (s SQLite) EvalExpression(line string) string {
-	db, err := sql.Open("sqlite3", ":memory:")
-	if err != nil {
-		panic(err)
+func (s *SQLite) EvalExpression(line string) string {
+	if s.db == nil {
+		s.Open()
 	}
 
-	_, err = db.Exec(line)
-	// rows, err := s.db.Query(line)
+	rows, err := s.db.Query(line)
 	if err != nil {
 		return err.Error()
 	}
-	return "evaled"
+	defer rows.Close()
+
+	//
+	// turn returned rows into a string
+	//
+	b := strings.Builder{}
+
+	// first set up the table header
+	cols, err := rows.Columns()
+	if err != nil {
+		panic(err)
+	}
+	for i, col := range cols {
+		b.WriteString(col)
+		if i < len(cols) - 1 {
+			b.WriteString("|")
+		} else if i == len(cols) - 1 {
+			b.WriteString("\n")
+		}
+	}
+
+	// write a divider
+	width := b.Len() - 1
+	for i := 0; i < width; i++ {
+		b.WriteString("-")
+	}
+	b.WriteString("\n")
+
+	// then slurp up and print a value for each column
+	for rows.Next() {
+		vals := make([]interface{}, len(cols))
+
+		// make a bunch of pointers to those vals for scanning
+		valPtrs := make([]interface{}, len(vals))
+		for i := range vals {
+			valPtrs[i] = &vals[i]
+		}
+		err := rows.Scan(valPtrs...)
+		if err != nil {
+			panic(err)
+		}
+
+		for i, val := range vals {
+			// convert byte slices to str
+			if s, ok := val.([]byte); ok {
+				b.WriteString(string(s))
+			} else {
+				b.WriteString(fmt.Sprintf("%v", val))
+			}
+
+			if i < len(vals) - 1 {
+				b.WriteString("|")
+			} else if i == len(vals) - 1 {
+				b.WriteString("\n")
+			}
+		}
+	}
+
+	// TODO: I _think_ any error here would be unrelated to the provided expression,
+	// but will need to revisit and dig into what .Err() actually checks.
+	if err = rows.Err(); err != nil {
+		panic(err)
+	}
+
+	// don't return the last newline
+	return b.String()[:b.Len()-1]
 }
