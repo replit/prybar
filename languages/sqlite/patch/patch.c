d2f09794a63e1fd3029f18ce8c57c14359a65165
Sidney Kochman <sidney@kochman.org>
2019-06-26 14:16:46 -0400

    basically works!

--- languages/sqlite/patch/patch.c
+++ languages/sqlite/patch/patch.c
@@ -7,38 +7,50 @@
 
 int (*orig_fprintf_chk)(FILE *out, int flag, const char *format, ...) = NULL;
 
+int env_checked = 0;
 int quiet;
 
 int __printf_chk(int flag, const char *format, ...) {
-    va_list arg;
-    va_start(arg, format);
-    int rc = __fprintf_chk(stdout, flag, format);
-    va_end(arg);
-    return rc;
-}
-
-int __fprintf_chk(FILE *out, int flag, const char *format, ...) {
-    if (!orig_fprintf_chk) {
-        orig_fprintf_chk = dlsym(RTLD_NEXT, "__fprintf_chk");
-
-        if (getenv("PRYBAR_QUIET")) {
-            quiet = 1;
-        } else {
-            quiet = 0;
-        }
-    }
-
-    if (quiet) {
+    if (prybar_quiet()) {
         return 0;
     }
 
     va_list arg;
     va_start(arg, format);
-    int rc = fprintf(out, format, arg);
+    int rc = internal_print(stdout, format, arg);
+    va_end(arg);
+    return rc;
+}
+
+int __fprintf_chk(FILE *out, int flag, const char *format, ...) {
+    if (prybar_quiet()) {
+        return 0;
+    }
+
+    va_list arg;
+    va_start(arg, format);
+    int rc = internal_print(out, format, arg);
     va_end(arg);
 
     // after we've done this once, we're done
     quiet = 0;
 
     return rc;
+}
+
+int internal_print(FILE *out, const char *format, va_list args) {
+    return vfprintf(out, format, args);
+}
+
+int prybar_quiet() {
+    if (env_checked)
+        return quiet;
+
+    if (getenv("PRYBAR_QUIET"))
+        quiet = 1;
+    else
+        quiet = 0;
+
+    env_checked = 1;
+    return quiet;
 }
