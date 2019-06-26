5102da8975a2e473ab08c08f1557af30a25af099
Sidney Kochman <sidney@kochman.org>
2019-06-26 13:18:24 -0400

    getting there

--- languages/sqlite/patch/patch.c
+++ languages/sqlite/patch/patch.c
@@ -5,14 +5,21 @@
 #include <stdlib.h>
 #include <string.h>
 
-int (*orig_printf_chk)(int flag, const char *format, ...) = NULL;
-int (*orig_process_input)(void *state, FILE *in) = NULL;
+int (*orig_fprintf_chk)(FILE *out, int flag, const char *format, ...) = NULL;
 
 int quiet;
 
 int __printf_chk(int flag, const char *format, ...) {
-    if (!orig_printf_chk) {
-        orig_printf_chk = dlsym(RTLD_NEXT, "__printf_chk");
+    va_list arg;
+    va_start(arg, format);
+    int rc = __fprintf_chk(stdout, flag, format);
+    va_end(arg);
+    return rc;
+}
+
+int __fprintf_chk(FILE *out, int flag, const char *format, ...) {
+    if (!orig_fprintf_chk) {
+        orig_fprintf_chk = dlsym(RTLD_NEXT, "__fprintf_chk");
 
         if (getenv("PRYBAR_QUIET")) {
             quiet = 1;
@@ -27,7 +34,7 @@ int __printf_chk(int flag, const char *format, ...) {
 
     va_list arg;
     va_start(arg, format);
-    int rc = vprintf(format, arg);
+    int rc = fprintf(out, format, arg);
     va_end(arg);
 
     // after we've done this once, we're done
@@ -35,16 +42,3 @@ int __printf_chk(int flag, const char *format, ...) {
 
     return rc;
 }
-
-// the first time this is called, we know that we should no longer suppress any
-// output
-// int process_input(void *state, FILE *in) {
-//     printf("hey\n");
-//     if (!orig_process_input) {
-//         orig_process_input = dlsym(RTLD_NEXT, "process_input");
-//     }
-
-//     passthrough = 1;
-
-//     return orig_process_input(state, in);
-// }
