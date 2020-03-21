23178766082a694b68a0c366e84d7e34fcbd1cb3
Salam Elbilig <abdusalam@live.com>
2020-03-20 22:36:47 -0700

    use tools.namespace for hot-reloading namespaces
    Namespaces that have had code changes in them can then be reloaded/refreshed
    by executing `((requiring-resolve 'clojure.tools.namespace.repl/refresh))`
    in the REPL, without restarting the REPL JVM process. Dependent namespaces of
    the changed namespaces will also be automatically reloaded/refreshed.

--- languages/clojure/main.go
+++ languages/clojure/main.go
@@ -1,103 +1,80 @@
 package main
 
-//go:generate ../../scripts/gofiles.sh generated_files.go
-
 import (
 	"fmt"
-	"io/ioutil"
 	"os"
 	"os/exec"
 	"syscall"
 	"github.com/replit/prybar/utils"
 )
 
-func findHelper(path string) string {
-    fileName := path + ".clj"
-	bytes, err := File(fileName)
-	if bytes != nil {
-		f, err := ioutil.TempFile("", fileName)
-		if err != nil {
-			panic(err)
-		}
-
-		if _, err = f.Write(bytes); err != nil {
-			panic(err)
-		}
-		if err = f.Close(); err != nil {
-			panic(err)
-		}
-		return f.Name()
-	}
-	if err != nil {
-		panic(err)
-	}
-    panic("File not found: " + fileName)
-}
-
 func Execute(config *utils.Config) {
-	path, err := exec.LookPath("clj")
+	cljPath, err := exec.LookPath("clj")
 
 	if err != nil {
 		panic(err)
 	}
 
 	env := os.Environ()
-	args := []string{"clj"}
+	args := []string{"clj", "-Sdeps", "{:deps {org.clojure/tools.namespace {:mvn/version \"1.0.0\"}} :paths [\"src\" \".\"]}"}
 
 	hasOption := config.Code != "" || config.Exp != "" ||
 		config.Interactive || config.OurInteractive
-	hasFile := false
-
-	if config.Ps1 != "" {
-		args = append(args, "-J-DPRYBAR_PS1=" + config.Ps1)
-	}
-
-	if config.Ps2 != "" {
-		args = append(args, "-J-DPRYBAR_PS2=" + config.Ps2)
-	}
+	filePath := ""
 
 	if config.Quiet {
-		args = append(args, "-J-DPRYBAR_QUIET=true")
+		// An empty eval-opt suppresses the greeting (e.g., "Clojure 1.10.1")
+		args = append(args, "--eval", "")
 	}
 
-	if hasOption {
-		if config.Args != nil && len(config.Args) > 0 {
-			if _, err := os.Stat(config.Args[0]); os.IsNotExist(err) {
-				fmt.Println("No such file:", config.Args[0])
-				os.Exit(2)
-			}
-
-			hasFile = true
-		}
-
-		if hasFile {
-			args = append(args, "--init", config.Args[0])
-		}
-
-		if config.Code != "" {
-			// "--eval" prints non-nil results only.
-			effect := fmt.Sprintf("(do %s nil)", config.Code)
-			args = append(args, "--eval", effect)
-		}
-
-		if config.Exp != "" {
-			args = append(args, "--eval", config.Exp)
-		}
-
-		if config.Interactive || config.OurInteractive {
-			args = append(args, findHelper("prybar_repl"))
-		}
-
-		if hasFile {
-			args = append(args, config.Args[1:]...)
-		} else {
-			args = append(args, config.Args...)
-		}
-	} else {
+	if !hasOption {
+		// An empty eval-opt suppresses the start of a REPL.
 		args = append(args, "--eval", "")
+		args = append(args, config.Args...)
+		syscall.Exec(cljPath, args, env)
+
+		return
+	}
+
+	if config.Args != nil && len(config.Args) > 0 {
+		if _, err := os.Stat(config.Args[0]); os.IsNotExist(err) {
+			fmt.Println("No such file:", config.Args[0])
+			os.Exit(2)
+		}
+
+		filePath = config.Args[0]
+	}
+
+	if config.Code != "" {
+		// "--eval" prints non-nil results only.
+		effect := fmt.Sprintf("(do %s nil)", config.Code)
+		args = append(args, "--eval", effect)
+	}
+
+	if config.Exp != "" {
+		args = append(args, "--eval", config.Exp)
+	}
+
+	if !(config.Interactive || config.OurInteractive) {
+		args = append(args, config.Args...)
+		syscall.Exec(cljPath, args, env)
+
+		return
+        }
+
+	// Starting a REPL, pass the file along, if exists, as an init-opt.
+	if filePath != "" {
+		args = append(args, "--init", filePath)
+	}
+
+	args = append(args, "--repl")
+
+	if filePath != "" {
+		args = append(args, config.Args[1:]...)
+	} else {
 		args = append(args, config.Args...)
 	}
 
-	syscall.Exec(path, args, env)
+	syscall.Exec(cljPath, args, env)
 }
 
