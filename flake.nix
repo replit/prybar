c9622969f5f51952c49e41bdc089042ac357a7bf
Toby Ho <toby@repl.it>
2022-12-08 17:09:00 -0500

    removed unused langs; created dev shell; updated nix channel to 22.11

--- flake.nix
+++ flake.nix
@@ -19,35 +19,11 @@
         buildPrybar = attrs:
           pkgs.callPackage (import ./build-prybar.nix attrs) { inherit rev; };
 
-        julia = import ./languages/julia/julia.nix { inherit pkgs; };
-
-        clojureWithCP =
-          import ./languages/clojure/wrappedClojure.nix { inherit pkgs; };
-        
-        python310Full = pkgs.python310.override {
-          self = python310Full;
-          pythonAttr = "python310Full";
-          bluezSupport = true;
-          x11Support = true;
-        };
-
       in {
         packages = {
-          prybar-R = buildPrybar {
-            language = "R";
-            buildInputs = [ pkgs.R ];
-            pkgName = "libR";
-            setFlags = true;
-          };
-
           prybar-nodejs = buildPrybar {
             language = "nodejs";
             binaries = [ pkgs.nodejs-16_x ];
-          };
-
-          prybar-python2 = buildPrybar {
-            language = "python2";
-            buildInputs = [ pkgs.python2 ];
           };
 
           prybar-python3 = buildPrybar {
@@ -57,36 +33,13 @@
 
           prybar-python310 = buildPrybar {
             language = "python310";
-            buildInputs = [ python310Full ];
+            buildInputs = [ pkgs.python310Full ];
           };
 
           prybar-lua = buildPrybar {
             language = "lua";
             buildInputs = [ pkgs.lua5_1 pkgs.readline ];
           };
-
-          prybar-clojure = buildPrybar {
-            language = "clojure";
-            buildInputs = [ clojureWithCP ];
-            binaries = [ pkgs.jdk11_headless ];
-          };
-
-          prybar-elisp = buildPrybar { language = "elisp"; };
-
-          prybar-ocaml = buildPrybar {
-            language = "ocaml";
-            # This has to be in the build inputs so it set-ups the library paths before the check phase
-            # It will not be copied over to the final package.
-            buildInputs = [
-              pkgs.opam
-              pkgs.ocaml
-              pkgs.ocamlPackages.findlib
-              pkgs.ocamlPackages.topkg
-              pkgs.ocamlPackages.reason
-            ];
-          };
-
-          prybar-scala = buildPrybar { language = "scala"; };
 
           prybar-sqlite = buildPrybar {
             language = "sqlite";
@@ -97,26 +50,32 @@
             language = "tcl";
             buildInputs = [ pkgs.tcl ];
           };
-        }
-        # These packages have issues on macOS
-          // nixpkgs.lib.optionalAttrs (!pkgs.stdenv.isDarwin) {
-            prybar-julia = buildPrybar {
-              language = "julia";
-              buildInputs = [ julia ];
-              setFlags = true;
-              binaries = [ pkgs.zlib ];
-            };
+        };
 
-            # The official Ruby packages is running ruby 2.7
-            prybar-ruby = buildPrybar {
-              language = "ruby";
-              buildInputs = [ pkgs.ruby ];
-              pkgName = "ruby-2.7";
-              setFlags = true;
-            };
-
-          };
-
+        devShells.default = pkgs.mkShell {
+          nativeBuildInputs = [
+            pkgs.pkg-config
+          ];
+          buildInputs = [
+            pkgs.libxcrypt
+            pkgs.nodejs-16_x
+            pkgs.python2
+            pkgs.python38Full
+            pkgs.python310Full
+            pkgs.zlib
+            pkgs.lua5_1
+            pkgs.readline
+            pkgs.sqlite
+            pkgs.tcl
+            pkgs.expect
+          ];
+          shellHook = ''
+            export CGO_LDFLAGS_ALLOW="-Wl,--compress-debug-sections=zlib"
+            export DISABLE_GO_TESTS=1
+          '';
+        };
       });
+
+      
 }
 
--- languages/ruby/pry_ruby.h
+++
@@ -1,8 +0,0 @@
-#include <ruby.h>
-#include <ruby/version.h>
-
-void pry_open();
-const char *pry_ruby_version();
-char *pry_eval(const char *code);
-int pry_eval_file(const char *file);
-
